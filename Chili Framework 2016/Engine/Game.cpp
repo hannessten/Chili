eb71325ca3fd3cc2dc8851a654b962bc710c4e60
Johannes Stenbäck <hannessten@msn.com>
2018-12-07 21:58:57 +0100

    added files from GDrive

--- Chili Framework 2016/Engine/Game.cpp
+++ Chili Framework 2016/Engine/Game.cpp
@@ -38,8 +38,103 @@ void Game::Go()
 
 void Game::UpdateModel()
 {
+	if (wnd.kbd.KeyIsPressed(VK_RIGHT))
+	{
+		x_mobile = x_mobile + 1;
+	}
+
+	if (wnd.kbd.KeyIsPressed(VK_LEFT))
+	{
+		x_mobile = x_mobile - 1;
+	}
+
+	if (wnd.kbd.KeyIsPressed(VK_UP))
+	{
+		y_mobile = y_mobile - 1;
+	}
+
+	if (wnd.kbd.KeyIsPressed(VK_DOWN))
+	{
+		y_mobile = y_mobile + 1;
+	}
+
+	if (x_mobile + 5 >= gfx.ScreenWidth)
+	{
+		x_mobile = gfx.ScreenWidth - 6;
+	}
+	if (x_mobile - 5 < 0)
+	{
+		x_mobile = 5;
+	}
+	if (y_mobile + 5 >= gfx.ScreenHeight)
+	{
+		y_mobile = gfx.ScreenHeight - 6;
+	}
+	if (y_mobile - 5 < 0)
+	{
+		y_mobile = 5;
+	}
+
+	colliding = OverlapTest(x_fixed, y_fixed, x_mobile, y_mobile);
 }
 
 void Game::ComposeFrame()
 {
+	DrawBox(x_fixed, y_fixed, 0,255,0);
+
+	if (colliding)
+	{
+		DrawBox(x_mobile, y_mobile, 255, 0, 0);
+	}
+	else
+	{
+		DrawBox(x_mobile, y_mobile, 255, 255, 255);
+	}
+
+}
+
+void Game::DrawBox(int x, int y, int r, int g, int b)
+{
+	// rita ut shapeen
+	gfx.PutPixel(x - 5, y - 5, r, g, b);
+	gfx.PutPixel(x - 5, y - 4, r, g, b);
+	gfx.PutPixel(x - 5, y - 3, r, g, b);
+	gfx.PutPixel(x - 4, y - 5, r, g, b);
+	gfx.PutPixel(x - 3, y - 5, r, g, b);
+	gfx.PutPixel(x - 5, y + 5, r, g, b);
+	gfx.PutPixel(x - 5, y + 4, r, g, b);
+	gfx.PutPixel(x - 5, y + 3, r, g, b);
+	gfx.PutPixel(x - 4, y + 5, r, g, b);
+	gfx.PutPixel(x - 3, y + 5, r, g, b);
+	gfx.PutPixel(x + 5, y - 5, r, g, b);
+	gfx.PutPixel(x + 5, y - 4, r, g, b);
+	gfx.PutPixel(x + 5, y - 3, r, g, b);
+	gfx.PutPixel(x + 4, y - 5, r, g, b);
+	gfx.PutPixel(x + 3, y - 5, r, g, b);
+	gfx.PutPixel(x + 5, y + 5, r, g, b);
+	gfx.PutPixel(x + 5, y + 4, r, g, b);
+	gfx.PutPixel(x + 5, y + 3, r, g, b);
+	gfx.PutPixel(x + 4, y + 5, r, g, b);
+	gfx.PutPixel(x + 3, y + 5, r, g, b);
+
+}
+
+bool Game::OverlapTest(int box0x, int box0y, int box1x, int box1y)
+{
+	// gränserna för varje box
+	const int left_box0 = box0x - 5;
+	const int right_box0 = box0x + 5;
+	const int top_box0 = box0y - 5;
+	const int bottom_box0 = box0y + 5;
+
+	const int left_box1 = box1x - 5;
+	const int right_box1 = box1x + 5;
+	const int top_box1 = box1y - 5;
+	const int bottom_box1 = box1y + 5;
+
+	// koll ifall boxarna överlappar
+	return (left_box0 <= right_box1 &&
+		right_box0 >= left_box1 &&
+		top_box0 <= bottom_box1 &&
+		bottom_box0 >= top_box1);
 }
